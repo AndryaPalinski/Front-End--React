598c7efc01b53d4ce19f0ba08899e655e022d661
Andrya <130005796+KatteKitty@users.noreply.github.com>
2023-11-05 23:54:21 -0300

    Tudo funfando :D (Sem node_modules)

---
+++ ReactJS-projetoTeste2/src/pages/primeiroComponente/Todo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Todo from "../../components/form";
+
+
+function Inicial() {
+    return (
+        <article className="card">
+            <div>
+                <Todo/>
+            </div>
+        </article>
+    );
+};
+
+export default Inicial 
